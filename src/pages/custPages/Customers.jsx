69747ec7b8745acadf801450540c91fb4bfb1c61
KareemAbo3id <kareemabo3id@hotmail.com>
2023-01-11 23:21:32 +0300

    #5: test commit

--- src/pages/custPages/Customers.jsx
+++ src/pages/custPages/Customers.jsx
@@ -3,11 +3,11 @@ import { DataGrid } from '@mui/x-data-grid';
 
 // Table Structure:
 const columns = [
-  { field: 'id', headerName: '#', },
-  { field: 'custName', headerName: 'customer name', },
-  { field: 'salesman', headerName: 'salesman', },
-  { field: 'region', headerName: 'region', },
-  { field: 'credit', headerName: 'credit', },
+  { field: 'id', headerName: '#' },
+  { field: 'custName', headerName: 'customer name' },
+  { field: 'salesman', headerName: 'salesman' },
+  { field: 'region', headerName: 'region' },
+  { field: 'credit', headerName: 'credit' },
 ];
 
 // UI FUNCTION -----------------------
@@ -15,14 +15,20 @@ export default function Customers() {
   const [customers, setCustomers] = useState([]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://raw.githubusercontent.com/KareemAbo3id/erp-react-app/master/db.json')
       .then(res => res.json())
       .then(cust => setCustomers(cust));
   }, []);
 
   return (
     <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={customers} columns={columns} pageSize={5} rowsPerPageOptions={[5]} checkboxSelection />
+      <DataGrid
+        rows={customers.map(i => i)}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
     </div>
   );
 }
